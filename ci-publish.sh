2a41cae61aa2ebd59ea2fdec8f5eb8a81a25dd73
Ouyang Jichao <oyanglulu@gmail.com>
2015-02-27 23:54:11 +0800

    not force publish

--- ci-publish.sh
+++ ci-publish.sh
@@ -1,5 +1,4 @@
 #!/bin/sh
 export NAME='Jichao Ouyang'
-git clone git@github.com:jcouyang/blog.git -b source org
 git clone git@github.com:jcouyang/blog.git -b gh-pages public
-emacs --batch -l emacs.el -f org-publish-all
+emacs --batch -l emacs.el org-publish-all
