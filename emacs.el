477acb0222b123e0c77c7d49929582781bb57af3
Jichao Ouyang <oyanglulu@gmail.com>
2015-02-28 09:24:32 +0800

    add cache for org time stamp

--- emacs.el
+++ emacs.el
@@ -11,6 +11,7 @@
 (clojure-font-lock-setup)
 (require 'org)
 (require 'ox-org)
+(require 'ox-rss)
 (require 'htmlize)
 (setq blog-path (expand-file-name "org"))
 (setq org-html-validation-link nil)
@@ -34,12 +35,16 @@
         :section-numbers nil
         :with-toc t
         :html-link-up "/index.html"
-        :html-link-home "/~jichao.ouyang.html"
+        :html-link-home "https://oyanglul.us/~jichao.ouyang.html"
         :auto-preamble t
         :html-postamble-format "%a %d" ;write author and date at end
         :auto-sitemap t
         :sitemap-title "Jichao Ouyang's Blog"
         :sitemap-filename "index"
+        :auto-index t
+        :index-filename "sitemap.org"
+        :index-title "Jichao Ouyang's Blog"
+        :index-function org-publish-org-index
         :sitemap-sort-files anti-chronologically
         :sitemap-file-entry-format "%t (%d)" ;write title and date in sitemap
         :html-head-include-default-style nil
@@ -53,7 +58,18 @@
          :recursive t
          :publishing-function org-publish-attachment
          )
-
-       ("blog" :components ("blog-notes" "blog-static"))
+       ("blog-rss"
+         :base-directory ,blog-path
+         :base-extension "org"
+         :rss-image-url "http://steckerhalter.co.vu/img/steckerhalter.png"
+         :publishing-directory "public"
+         :publishing-function org-rss-publish-to-rss
+         :section-numbers nil
+         :html-link-home "https://blog.oyanglul.us/"
+         :html-link-use-abs-url t
+         :with-toc nil
+         :exclude ".*"
+         :include ("index"))
+       ("blog" :components ("blog-notes" "blog-static" "blog-rss"))
        )))
 (set-org-publish-project-alist)
