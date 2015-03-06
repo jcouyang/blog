83889543ce1f79816b6e98628dfda81a196a85ed
Jichao Ouyang <oyanglulu@gmail.com>
2015-03-06 14:08:49 +0800

    rm cd

--- import.sh
+++ import.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
-cd markdown
 for file in $(find blog-master -iname "*.md")
   do
-  	pandoc $file -f markdown -t org -o ../org/${file}.org
+  	pandoc $file -f markdown -t org -o org/${file}.org
 done
