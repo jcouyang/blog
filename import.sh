5a06b3f46949bc8ae7461c7b55e174e08939da8a
Jichao Ouyang <oyanglulu@gmail.com>
2015-03-08 22:09:47 +0800

    mv markdown under org folder

--- import.sh
+++ import.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
+cp -r blog-master/org/* org/
+
 for file in $(find blog-master -iname "*.md")
   do
-  	pandoc $file -f markdown -t org -o ${file}.org
-    cp ${file}.org org/
+      pandoc $file -f markdown -t org -o ${file}.org
+      touch -r $file ${file}.org
 done
 
-cp -r blog-master/org/* org/
+
