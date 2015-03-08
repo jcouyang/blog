051978d74786d38f07263027f220c317e66f8204
Jichao Ouyang <oyanglulu@gmail.com>
2015-03-08 22:41:57 +0800

    pandoc all md file in org folder

--- import.sh
+++ import.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 cp -r blog-master/org/* org/
 
-for file in $(find blog-master -iname "*.md")
+for file in $(find org -iname "*.md")
   do
       pandoc $file -f markdown -t org -o ${file}.org
       touch -r $file ${file}.org
