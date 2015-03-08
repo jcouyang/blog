7e49d696c03870a0de7c680dc59f0fd952913408
Jichao Ouyang <oyanglulu@gmail.com>
2015-03-08 23:05:00 +0800

    rm org folder replace with from zip file

--- import.sh
+++ import.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
-cp -r blog-master/org/* org/
+rm -rf org
+mv blog-master/org org
+ls -al org
 
 for file in $(find org -iname "*.md")
   do
       pandoc $file -f markdown -t org -o ${file}.org
       touch -r $file ${file}.org
+      echo "convert markdown $file into org"
 done
 
 ls -al org/
