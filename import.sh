5679044cdaa8e41726c6e794f855016cb05e78c8
Jichao Ouyang <oyanglulu@gmail.com>
2015-03-08 23:30:15 +0800

    extract preserve timestamp sh

--- import.sh
+++ import.sh
@@ -1,10 +1,4 @@
-#!/bin/bash
-for FILE in $(git ls-files)
-do
-    TIME=$(git log --pretty=format:%cd -n 1 --date=iso $FILE)
-    TIME=$(date -j -f '%Y-%m-%d %H:%M:%S %z' "$TIME" +%Y%m%d%H%M.%S)
-    touch -m -t $TIME $FILE
-done
+#!/bin/bash -e
 
 for file in $(find org -iname "*.md")
   do
