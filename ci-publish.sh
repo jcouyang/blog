5e2d907c7d7548dbbb0b37f48682d9bc2f73f291
Jichao Ouyang <oyanglulu@gmail.com>
2015-03-21 23:26:52 +0800

    run emacs again to compile archive.org

--- ci-publish.sh
+++ ci-publish.sh
@@ -2,3 +2,5 @@
 export NAME='Jichao Ouyang'
 git clone git@github.com:jcouyang/blog.git -b gh-pages public
 cask exec emacs --batch -l emacs.el -f org-publish-all
+# run it again for archive.org
+cask exec emacs --batch -l emacs.el -f org-publish-all
