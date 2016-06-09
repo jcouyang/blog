22b9b784b9c7f573c6e5b77b6bd5ec9ed7fd764d
Jichao Ouyang <oyanglulu@gmail.com>
2016-06-10 00:48:32 +0800

    fix circle

--- ci-publish.sh
+++ ci-publish.sh
@@ -1,6 +1,13 @@
 #!/bin/sh
 export NAME='Jichao Ouyang'
+export PATH="/home/ubuntu/.cask/bin:$PATH"
 git clone git@github.com:jcouyang/blog.git -b gh-pages public
 cask exec emacs --batch -l emacs.el -f org-publish-all
 # run it again for archive.org
 cask exec emacs --batch -l emacs.el -f org-publish-all
+cd public
+git config --global user.name "circle ci"
+git config --global user.email "oyanglulu@gmail.com"
+git add .
+git commit -m "auto publi:ship:"
+git push origin gh-pages
