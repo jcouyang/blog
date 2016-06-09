c1811f7ffb453af825958711a4ff40e414f63ca8
Jichao Ouyang <oyanglulu@gmail.com>
2016-06-10 01:23:40 +0800

    update readme and commit msg

--- ci-publish.sh
+++ ci-publish.sh
@@ -9,5 +9,5 @@ cd public
 git config --global user.name "circle ci"
 git config --global user.email "oyanglulu@gmail.com"
 git add .
-git commit -m "auto publi:ship:"
+git commit -m "auto publi:ship: [ci skip]"
 git push origin gh-pages
