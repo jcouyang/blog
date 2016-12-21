dcef145243b2d57ce363c22375312037e0b72863
Jichao Ouyang <oyanglulu@gmail.com>
2016-12-21 23:30:14 +0800

    dockerize

--- ci-publish.sh
+++ ci-publish.sh
@@ -1,11 +1,7 @@
 #!/bin/sh
 set -e
 export NAME='Jichao Ouyang'
-export PATH="/home/ubuntu/.cask/bin:$PATH"
-git clone git@github.com:jcouyang/blog.git -b gh-pages public
-cask exec emacs --batch -l emacs.el -f org-publish-all
-# run it again for archive.org
-cask exec emacs --batch -l emacs.el -f org-publish-all
+
 cd public
 git config --global user.name "circle ci"
 git config --global user.email "oyanglulu@gmail.com"
