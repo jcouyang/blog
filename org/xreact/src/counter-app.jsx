19ac3a8bb12109526ae7007b37689c11c51d7311
Jichao Ouyang <oyanglulu@gmail.com>
2017-12-09 19:55:13 +0800

    clean up a little bit

--- org/xreact/src/counter-app.jsx
+++
@@ -1,8 +0,0 @@
-import * as React from 'react'
-import { render } from 'react-dom';
-import {Counter} from './components/counter'
-import {X} from 'xreact'
-import * as RX from 'xreact/lib/xs/rx'
-const xmount = (component, dom) => render(React.createFactory(X)({ x: RX }, component), dom)
-
-xmount(Counter, document.getElementById('counter-app'))
--- org/xreact/src/views/counter.js
+++
@@ -1,9 +0,0 @@
-import * as React from 'react'
-export const View = props => (
- <div>
-    <input type="button" name="dec" onClick={(e)=>props.actions.fromEvent(e)} value="-" />
-    <input type="number" name="count" value={props.count} onChange={props.actions.fromEvent} />
-    <input type="button" name="inc" onClick={(e)=>props.actions.fromEvent(e)} value="+" />
- </div>
-)
-View.defaultProps = {count: 0}
